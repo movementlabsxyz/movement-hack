97f5e6ffbb88b29a2e4ad77d96249ea9992050f5
Liam Monninger <79056955+l-monninger@users.noreply.github.com>
2023-12-04 11:54:06 -0800

    feat: update for new CLI

--- examples/movement/hello_blockchain/bin/run.sh
+++ examples/movement/hello_blockchain/bin/run.sh
@@ -11,30 +11,30 @@ function finish() {
 }
 
 begin "Funding account for hello_blockchain deployment and call..."
-movement account fund-with-faucet --account default #? does the same as the below
+movement aptos account fund-with-faucet --account default #? does the same as the below
 # movement faucet #? does the same as the above
 finish "Funded account for hello_blockchain deployment and call!"
 
 begin "Compiling hello_blockchain module..."
-echo "y" | movement move compile --named-addresses hello_blockchain=default
+echo "y" | movement aptos move compile --named-addresses hello_blockchain=default
 finish "Compiled hello_blockchain module!"
 
 begin "Publishing hello_blockchain module..."
-echo "y" | movement move publish --named-addresses hello_blockchain=default
+echo "y" | movement aptos move publish --named-addresses hello_blockchain=default
 finish "Published hello_blockchain module!"
 
 begin "Setting hello_blockchain message to 'hello!'..."
-echo "y" | movement move run --function-id default::message::set_message --args string:hello!
+echo "y" | movement aptos move run --function-id default::message::set_message --args string:hello!
 finish "Set hello_blockchain message to 'hello'!"
 
 begin "Querying resources for account..."
-movement account list --query resources --account default
+movement aptos account list --query resources --account default
 finish "Queried resources for account!"
 
 begin "Setting hello_blockchain message to 'goodbye!'..."
-echo "y" | movement move run --function-id default::message::set_message --args string:goodbye!
+echo "y" | movement aptos move run --function-id default::message::set_message --args string:goodbye!
 finish "Set hello_blockchain message to 'goodbye'!"
 
 begin "Querying resources for account..."
-movement account list --query resources --account default
+movement aptos account list --query resources --account default
 finish "Queried resources for account!"
